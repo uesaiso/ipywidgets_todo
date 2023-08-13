e6aeb99fb5b70fb2b6df941bbd9daee934f2b2ed
uesaiso <ysmsuehr@gmail.com>
2023-08-13 14:06:58 +0900

    Turn Todo into a subclass of HasTraits

--- jupyter_sandbox/store/todo_store.py
+++ jupyter_sandbox/store/todo_store.py
@@ -1,33 +1,33 @@
-from dataclasses import dataclass
-
-from traitlets import HasTraits, List
+from traitlets import Bool, HasTraits, List, Unicode, validate
 
 
-@dataclass(frozen=True)
-class Todo:
-    text: str
-    completed: bool
+class Todo(HasTraits):
+    text = Unicode()
+    completed = Bool()
 
-    def __post_init__(self) -> None:
-        if not len(self.text) > 0:
+    @validate("text")
+    def _validate_text(self, proposal: dict) -> str:
+        if not len(proposal["value"]) > 0:
             raise ValueError("Todo text must be a non-empty string")
+        return proposal["value"]
 
 
 class TodoStore(HasTraits):
-    todos = List([])
+    todos = List(list[Todo]())
 
-    def add_todo(self, text: str, completed: bool = False) -> None:
-        self.todos = self.todos + [Todo(text=text, completed=completed)]
-
-    def toggle_todo(self, index: int) -> None:
-        old_todo = self.todos[index]
-        new_todo = Todo(old_todo.text, not old_todo.completed)
-        self.todos = [
-            new_todo if idx == index else todo for idx, todo in enumerate(self.todos)
-        ]
+    def add_todo(self, text: str, completed: bool = False) -> Todo:
+        new_todo = Todo(text=text, completed=completed)
+        self.todos = self.todos + [new_todo]
+        return new_todo
 
     def remove_todo(self, index: int) -> None:
         self.todos = [todo for idx, todo in enumerate(self.todos) if idx != index]
+
+    def toggle_todo(self, index: int) -> Todo:
+        todo = self.todos[index]
+        todo.completed = not todo.completed
+        self.todos[index] = todo
+        return todo
 
 
 todo_store = TodoStore()
