b5b9a03d2c51e2a5edc8a1d4c366760ce9b52cff
uesaiso <ysmsuehr@gmail.com>
2023-08-13 14:39:46 +0900

    Not working: diff_update

--- jupyter_sandbox/components/todo_list.py
+++ jupyter_sandbox/components/todo_list.py
@@ -1,7 +1,13 @@
-from ipywidgets import Text, VBox
+from collections.abc import Callable
 
-from ..store import TodoStore, todo_store
+from ipywidgets import Text, VBox, Widget
+
+from ..store import Todo, TodoStore, todo_store
 from .todo_item import TodoItem
+
+
+def create_todo_item(item: Todo, index: int) -> TodoItem:
+    return TodoItem(todo=item, index=index)
 
 
 class TodoList(VBox):
@@ -10,16 +16,43 @@ class TodoList(VBox):
         self.store = store
         self.new_todo_input = Text(placeholder="New Todo")
         self.new_todo_input.on_submit(self.add_todo)
-        self.children = [self.new_todo_input]
 
-        self.store.observe(self.update_todos, names="todos")
-        self.update_todos()
+        self.store.observe(self.update_ui, names="todos")
+        self.todo_list = [
+            TodoItem(todo, index=idx) for idx, todo in enumerate(self.store.todos)
+        ]
+        self.children = [self.new_todo_input] + self.todo_list
 
     def add_todo(self, text_widget: Text) -> None:
         self.store.add_todo(text_widget.value)
         self.new_todo_input.value = ""
 
-    def update_todos(self, change: dict = None) -> None:
-        self.children = [self.new_todo_input] + [
-            TodoItem(todo, index=idx) for idx, todo in enumerate(self.store.todos)
-        ]
+    def update_ui(self, change: dict) -> None:
+        self.children = [self.new_todo_input] + self.diff_widgets_list(
+            change, self.todo_list, create_todo_item
+        )
+
+    def diff_widgets_list(
+        self,
+        change: dict,
+        widget_list: list[Widget],
+        widget_factory: Callable[..., Widget],
+    ) -> list[Widget]:
+        if not isinstance(change["old"], list) and isinstance(change["new"], list):
+            raise ValueError("changed value must be a list")
+        new_list = widget_list.copy()
+        old_items = change["old"]
+        new_items = change["new"]
+
+        # Identify items to be removed
+        items_to_remove = [item for item in old_items if item not in new_items]
+        for item in items_to_remove:
+            idx = old_items.index(item)
+            new_list.pop(idx)
+
+        # Identify items to be added
+        items_to_add = [item for item in new_items if item not in old_items]
+        for item in items_to_add:
+            idx = new_items.index(item)
+            new_list.insert(idx, widget_factory(item, idx))
+        return new_list
